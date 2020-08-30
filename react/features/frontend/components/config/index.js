19416fa7aef1c81cd942ffa48739f73ff39c0382
benny_sudibyo <benny_sudibyo@outlook.com>
2020-08-31 00:14:46 +0800

    update

--- react/features/frontend/components/config/index.js
+++ react/features/frontend/components/config/index.js
@@ -5,6 +5,6 @@ export default {
     },
     bosh:'https://beta.meet.jit.si/http-bind', // FIXME: use xep-0156 for that
     clientNode:'http://jitsi.org/jitsimeet',
-    apiurl:'https://118.138.242.112:4000',
+    apiurl:'https://18.138.242.112:4000',
     serverurl:'https://18.138.242.112'
 }
